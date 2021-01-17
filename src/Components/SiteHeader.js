5e79da37bb4dd6ce1c7db3757b63858ae3b517da
aniledev <elinamcgill@gmail.com>
2021-01-17 12:04:20 -0500

    STYLES: add minimal styling to SiteHeader component

--- src/Components/SiteHeader.js
+++ src/Components/SiteHeader.js
@@ -3,6 +3,6 @@ import "../Styles/SiteHeader.css";
 
 export default class SiteHeader extends Component {
   render() {
-    return <div className="SiteHeader">Site Header</div>;
+    return <div className="SiteHeader">Google Books Search</div>;
   }
 }
